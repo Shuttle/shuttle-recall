24281dbdd13b5475ea49ee8c9b51643e941f5579
Eben <me@ebenroux.co.za>
2022-09-04 13:11:19 +0200

    deploy

---
+++ assets/index.md.a38662c3.js
@@ -0,0 +1,20 @@
+import { _ as _export_sfc, a as createStaticVNode, c as createElementBlock, b as createVNode, o as openBlock } from "./app.91cc6c76.js";
+var Home_vue_vue_type_style_index_0_scoped_true_lang = "";
+const _sfc_main$1 = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<section id="hero" data-v-0a805888></section><section id="highlights" class="vt-box-container" data-v-0a805888><div class="vt-box" data-v-0a805888><h2 data-v-0a805888>Framework Support</h2><div data-v-0a805888> Packages currently target <code data-v-0a805888>netstandard2.0</code> and <code data-v-0a805888>netstandard2.1</code> which means that they can be used with .NET Core 2.1+, .NET Framework 4.6.1+, and .NET 5.0+ </div></div><div class="vt-box" data-v-0a805888><h2 data-v-0a805888>Storage</h2><div data-v-0a805888> Since the storage of events is abstracted any relevant data store may be used. The default mechanism is a Sql Server database. </div></div><div class="vt-box" data-v-0a805888><h2 data-v-0a805888>Open Source</h2><div data-v-0a805888> These packages are free open source software licensed under the <a href="https://opensource.org/licenses/BSD-3-Clause" data-v-0a805888>3-Clause BSD License</a>. Pull requests are welcome. </div></div></section>', 2);
+function _sfc_render(_ctx, _cache) {
+  return _hoisted_1;
+}
+var Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-0a805888"]]);
+const __pageData = '{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home"},"headers":[],"relativePath":"index.md"}';
+const __default__ = {};
+const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export { __pageData, _sfc_main as default };
