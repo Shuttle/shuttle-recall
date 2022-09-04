999183ba93c077b5e5a2fa21d5a593d87bdfe075
Eben <me@ebenroux.co.za>
2022-09-04 13:22:45 +0200

    deploy

--- assets/index.md.6fd77056.lean.js
+++ assets/index.md.6fd77056.lean.js
@@ -1,11 +1,15 @@
-import { _ as _export_sfc, a as createStaticVNode, c as createElementBlock, b as createVNode, o as openBlock } from "./app.91cc6c76.js";
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode, b as createVNode } from "./app.91cc6c76.js";
 var Home_vue_vue_type_style_index_0_scoped_true_lang = "";
 const _sfc_main$1 = {};
-const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 2);
+const _hoisted_1 = { id: "hero" };
+const _hoisted_2 = /* @__PURE__ */ createStaticVNode("", 3);
+const _hoisted_5 = [
+  _hoisted_2
+];
 function _sfc_render(_ctx, _cache) {
-  return _hoisted_1;
+  return openBlock(), createElementBlock("section", _hoisted_1, _hoisted_5);
 }
-var Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-ce795734"]]);
+var Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-68ea3977"]]);
 const __pageData = '{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home"},"headers":[],"relativePath":"index.md"}';
 const __default__ = {};
 const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
