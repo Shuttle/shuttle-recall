255ba56c3bdf01d0c1096530a9f706c521882f97
Eben <me@ebenroux.co.za>
2022-09-04 13:09:36 +0200

    deploy

---
+++ assets/index.md.92805e55.lean.js
@@ -0,0 +1,19 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, b as createVNode } from "./app.91cc6c76.js";
+var Home_vue_vue_type_style_index_0_scoped_true_lang = "";
+const _sfc_main$1 = {};
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, "Test");
+}
+var Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-0eb57094"]]);
+const __pageData = '{"title":"Home","description":"","frontmatter":{"page":true,"title":"Home"},"headers":[],"relativePath":"index.md"}';
+const __default__ = {};
+const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export { __pageData, _sfc_main as default };
