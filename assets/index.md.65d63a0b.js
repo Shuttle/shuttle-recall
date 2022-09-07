1f65a78880c6c7e150bd7cd95e8403ea6f6a2a3c
Eben <me@ebenroux.co.za>
2022-09-07 11:11:58 +0200

    deploy

---
+++ assets/index.md.65d63a0b.js
@@ -0,0 +1,23 @@
+import { _ as _export_sfc, a as createStaticVNode, o as openBlock, c as createElementBlock, b as createVNode } from "./app.0ac7c232.js";
+const Home_vue_vue_type_style_index_0_scoped_45c0ca8b_lang = "";
+const _sfc_main$1 = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<div data-v-45c0ca8b><section id="hero" data-v-45c0ca8b><h1 class="tagline" data-v-45c0ca8b>.Net Event Sourcing</h1><p class="description" data-v-45c0ca8b> An event sourcing/projection mechanism that abstracts the storage of events. </p><p class="actions" data-v-45c0ca8b><a class="why" href="/shuttle-recall/why.html" data-v-45c0ca8b>Why?</a><a class="get-started" href="/shuttle-recall/getting-started.html" data-v-45c0ca8b> Get Started <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" data-v-45c0ca8b><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" data-v-45c0ca8b></path></svg></a></p></section><section id="highlights" class="vt-box-container" data-v-45c0ca8b><div class="vt-box" data-v-45c0ca8b><h2 data-v-45c0ca8b>Framework Support</h2><div data-v-45c0ca8b> Packages currently target <code data-v-45c0ca8b>netstandard2.0</code> and <code data-v-45c0ca8b>netstandard2.1</code> which means that they can be used with .NET Core 2.1+, .NET Framework 4.6.1+, and .NET 5.0+ </div></div><div class="vt-box" data-v-45c0ca8b><h2 data-v-45c0ca8b>Storage</h2><div data-v-45c0ca8b> Since the storage of events is abstracted any relevant data store may be used. The default mechanism is a Sql Server database. </div></div><div class="vt-box" data-v-45c0ca8b><h2 data-v-45c0ca8b>Open Source</h2><div data-v-45c0ca8b> These packages are free open source software licensed under the <a href="https://opensource.org/licenses/BSD-3-Clause" data-v-45c0ca8b>3-Clause BSD License</a>. Pull requests are welcome. </div></div></section></div>&gt; ', 2);
+function _sfc_render(_ctx, _cache) {
+  return _hoisted_1;
+}
+const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-45c0ca8b"]]);
+const __pageData = JSON.parse('{"title":"Shuttle.Recall","description":"","frontmatter":{"page":true,"title":"Shuttle.Recall"},"headers":[],"relativePath":"index.md"}');
+const __default__ = { name: "index.md" };
+const _sfc_main = Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export {
+  __pageData,
+  _sfc_main as default
+};
