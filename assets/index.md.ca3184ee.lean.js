5b696b50bb18726e51fac43cc3a8921c5a58b068
Eben <me@ebenroux.co.za>
2022-09-07 10:52:19 +0200

    deploy

--- assets/index.md.ca3184ee.lean.js
+++ assets/index.md.ca3184ee.lean.js
@@ -1,11 +1,11 @@
 import { _ as _export_sfc, a as createStaticVNode, o as openBlock, c as createElementBlock, b as createVNode } from "./app.0ac7c232.js";
-const Home_vue_vue_type_style_index_0_scoped_037334cb_lang = "";
+const Home_vue_vue_type_style_index_0_scoped_3112f008_lang = "";
 const _sfc_main$1 = {};
 const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 2);
 function _sfc_render(_ctx, _cache) {
   return _hoisted_1;
 }
-const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-037334cb"]]);
+const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-3112f008"]]);
 const __pageData = JSON.parse('{"title":"Shuttle.Recall","description":"","frontmatter":{"page":true,"title":"Shuttle.Recall"},"headers":[],"relativePath":"index.md"}');
 const __default__ = { name: "index.md" };
 const _sfc_main = Object.assign(__default__, {
