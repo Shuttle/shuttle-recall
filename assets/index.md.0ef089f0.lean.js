97e7ecaa68084e156895727e5f86541857780384
Eben <me@ebenroux.co.za>
2022-09-07 12:40:34 +0200

    deploy

--- assets/index.md.0ef089f0.lean.js
+++ assets/index.md.0ef089f0.lean.js
@@ -1,11 +1,14 @@
-import { _ as _export_sfc, a as createStaticVNode, o as openBlock, c as createElementBlock, b as createVNode } from "./app.0ac7c232.js";
-const Home_vue_vue_type_style_index_0_scoped_45c0ca8b_lang = "";
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode, b as createVNode } from "./app.0ac7c232.js";
+const Home_vue_vue_type_style_index_0_scoped_957cadb2_lang = "";
 const _sfc_main$1 = {};
 const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 2);
+const _hoisted_3 = [
+  _hoisted_1
+];
 function _sfc_render(_ctx, _cache) {
-  return _hoisted_1;
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
 }
-const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-45c0ca8b"]]);
+const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-957cadb2"]]);
 const __pageData = JSON.parse('{"title":"Shuttle.Recall","description":"","frontmatter":{"page":true,"title":"Shuttle.Recall"},"headers":[],"relativePath":"index.md"}');
 const __default__ = { name: "index.md" };
 const _sfc_main = Object.assign(__default__, {
