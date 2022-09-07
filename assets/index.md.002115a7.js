7f3228ad02fb8f188a2f76283ea2c0b72c179a1e
Eben <me@ebenroux.co.za>
2022-09-07 06:59:54 +0200

    deploy

---
+++ assets/index.md.002115a7.js
@@ -0,0 +1,27 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode, b as createVNode } from "./app.0ac7c232.js";
+const Home_vue_vue_type_style_index_0_scoped_2aeb2737_lang = "";
+const _sfc_main$1 = {};
+const _hoisted_1 = { id: "hero" };
+const _hoisted_2 = /* @__PURE__ */ createStaticVNode('<h1 class="tagline" data-v-2aeb2737>.Net Event Sourcing</h1><p class="description" data-v-2aeb2737> An event sourcing/projection mechanism that abstracts the storage of events. </p><p class="actions" data-v-2aeb2737><a class="why" href="/shuttle-recall/why.html" data-v-2aeb2737>Why?</a><a class="get-started" href="/shuttle-recall/getting-started.html" data-v-2aeb2737> Get Started <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" data-v-2aeb2737><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" data-v-2aeb2737></path></svg></a></p>', 3);
+const _hoisted_5 = [
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("section", _hoisted_1, _hoisted_5);
+}
+const Home = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__scopeId", "data-v-2aeb2737"]]);
+const __pageData = JSON.parse('{"title":"Shuttle.Recall","description":"","frontmatter":{"page":true,"title":"Shuttle.Recall"},"headers":[],"relativePath":"index.md"}');
+const __default__ = { name: "index.md" };
+const _sfc_main = Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", null, [
+        createVNode(Home)
+      ]);
+    };
+  }
+});
+export {
+  __pageData,
+  _sfc_main as default
+};
